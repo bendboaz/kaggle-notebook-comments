139c3939da8b236394df39f4d90d0ffef9b51b37
bendboaz <boaz.ben-dov@campus.technion.ac.il>
2022-07-04 15:36:37 +0300

    Added notebook loading

---
+++ src/constants.py
@@ -0,0 +1,4 @@
+CELL_TYPE = 'cell_type'
+MARKDOWN = 'markdown'
+CODE = 'code'
+SOURCE = 'source'
--- src/load_notebooks.py
+++ src/load_notebooks.py
@@ -1,7 +1,9 @@
 import json
+from collections import defaultdict
 from pathlib import Path
-from typing import Union, List, Mapping
+from typing import Union, List, Mapping, Any
 
+from src.constants import CELL_TYPE, MARKDOWN, CODE, SOURCE
 from src.types.notebook import Notebook, Cell
 
 
@@ -14,23 +16,33 @@ def read_notebook_file(path: Union[str, Path]) -> Notebook:
     with open(path, 'r') as f:
         notebook_dict = json.load(f)
 
-    noteobok_id = path.name.split('.')[0]
+    notebook_id = get_notebook_id(path)
 
-    code_cells = list(filter(lambda cell_id: notebook_dict['cell_type'] == 'code', notebook_dict['source']))
-    markdown_cells = list(filter(lambda cell_id: notebook_dict['cell_type'] == 'markdown', notebook_dict['source']))
+    type_to_cells = get_cell_type_mapping(notebook_dict)
 
-    cell_contents = notebook_dict['source']
+    cell_contents = notebook_dict[SOURCE]
 
-    code_cells = load_cells(cell_contents, code_cells)
-    md_cells = load_cells(cell_contents, markdown_cells)
+    code_cells = load_cells(cell_contents, type_to_cells[CODE])
+    md_cells = load_cells(cell_contents, type_to_cells[MARKDOWN])
 
     notebook = Notebook(
-        noteobok_id,
+        notebook_id,
         code_cells,
         md_cells,
     )
 
     return notebook
+
+
+def get_notebook_id(path):
+    return path.name.split('.')[0]
+
+
+def get_cell_type_mapping(notebook_dict: Mapping[str, Any]) -> Mapping[str, List[str]]:
+    type_to_cells = defaultdict(list)
+    for cell_id, cell_type in notebook_dict[CELL_TYPE].items():
+        type_to_cells[cell_type].append(cell_id)
+    return type_to_cells
 
 
 def load_cells(cell_contents: Mapping[str, str], cell_ids: List[str]) -> List[Cell]:
