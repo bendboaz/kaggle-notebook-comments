87acb35e9b737ff2b6e2149abd13b2bc6916190d
bendboaz <boaz.ben-dov@campus.technion.ac.il>
2022-07-06 00:59:47 +0300

    Added pydantic-based config

---
+++ src/config.py
@@ -0,0 +1,30 @@
+from typing import Optional, Any
+
+from pydantic import BaseSettings, Field
+from pydantic.typing import StrPath
+
+
+env_file_sentinel = str(object())
+
+
+class GTConfig(BaseSettings):
+    train_part: float
+    force_split: bool = Field(default=False)
+
+
+class ExperimentSettings(BaseSettings):
+    gt: GTConfig
+    experiment_name: str
+
+    class Config:
+        env_prefix = 'KAGGLE_NOTEBOOKS_'
+        env_nested_delimiter = '__'
+
+    def __init__(self,
+                 _env_file: Optional[StrPath] = env_file_sentinel,
+                 _env_file_encoding: Optional[str] = None,
+                 _env_nested_delimiter: Optional[str] = None,
+                 _secrets_dir: Optional[StrPath] = None,
+                 **values: Any):
+        super(ExperimentSettings, self).__init__(_env_file, _env_file_encoding, _env_nested_delimiter, _secrets_dir,
+                                                 **values)
